2c2e01271d82cfecbd2485d25af37489f316217c
Soulsaw <ssawadogo292@gmail.com>
2024-03-07 02:41:49 +0000

    add new test case

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
-def Additionner(a,b):
-	return a+b
+def additionner(a,b):
+	return (a + b)
+
+def soustraction(a, b):
+	return (a - b)
+
+def division(a, b):
+	if b == 0:
+		raise ZeroDivisionError("Imposible de diviser par 0")
+	return (a / b)
+
+def multiplication(a, b):
+	return (a * b)
